79e58cd3518dec0dc33e036a1f98c86d06791f19
jhmk1 <mkwang1014@gmail.com>
2022-12-11 18:49:47 +0900

    dhsmfdms

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import display.*;
 
 public class Main {
     public static void main(String[] args) {
-        new Login(); // 프로그램 시작
+        new Login();
+        //System.out//// 프로그램 시작
     }
 }
